60f2c5688cefa19e19cd982d3f2a504c5dac5a8d
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-08-30 22:45:55 +0200

    fix errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
+function newItem(){
 
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
 
-if (inputValue === '') {
-    alert('You must write something!');
-} else {
-    let list = $('#list');
-    $(list).append(li);
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        let list = $('#list');
+        $(list).append(li);
+    }
+
+    function crossOut() {
+        $(li).toggleClass('strike');
+    }
+
+    li.on('dbclick', function crossOut (){
+        li.toggleClass('strike');
+    });
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", deleteListItem);
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+
+    $('#list').sortable();
 }
-
-function crossOut() {
-    $(li).toggleClass('strike');
-}
-
-li.on('dbclick', function crossOut (){
-    li.toggleClass('strike');
-});
-
-let crossOutButton = $(<crossOutButton></crossOutButton>);
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
-
-crossOutButton.on("click", deleteListItem);
-function deleteListItem() {
-    li.addClass('delete');
-}
-
-$('#list').sortable();
-
-
