4d360ca1c4dc852eb5ae86b2065adb44e819ab3b
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-08-30 17:20:25 +0200

    add new item to the list

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,12 @@ function newItem() {
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        let list = $('#list');
+        $(list).append(li);
+    }
 }
 
