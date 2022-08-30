98f78cc902d9a654d45c5f2723e5ac79aeb01b97
mihadereani <110335459+mihadereani@users.noreply.github.com>
2022-08-30 22:12:10 +0200

    crossing out an item

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
-function newItem() {
-    let li = $('<li></li>');
-    let inputValue = $('#input').val();
-    li.append(inputValue);
 
-    if (inputValue === '') {
-        alert('You must write something!');
-    } else {
-        let list = $('#list');
-        $(list).append(li);
-    }
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
+
+if (inputValue === '') {
+    alert('You must write something!');
+} else {
+    let list = $('#list');
+    $(list).append(li);
 }
+
+function crossOut() {
+    $(li).toggleClass('strike');
+}
+
+li.on('dbclick', function crossOut (){
+    li.toggleClass('strike');
+  });
 
